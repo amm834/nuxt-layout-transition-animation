6b8568c74d353d49017ae5e949ba788fadc3342f
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-04 20:06:11 +0630

    feat: add nuxt layout transition

---
+++ pages/posts/[id].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useRoute} from "#app";
+
+const route = useRoute();
+
+</script>
+
+<template>
+    Hello Post {{ route.params.id}}
+</template>
+
+<style scoped>
+
+</style>
